OCA/account-payment

[IMP] account_payment_term_extension: Add amount_untaxed to value option in payment term lines

account_payment_term_extension/models/account_move.py
@@ -1,0 +1,14 @@
+# Copyright 2021 Tecnativa Víctor Martínez
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+from odoo import models
+
+
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    def _recompute_payment_terms_lines(self):
+        super(
+            AccountMove, self.with_context(last_stock_move=self)
+        )._recompute_payment_terms_lines()

account_payment_term_extension/models/account_payment_term.py
@@ -3,6 +3,7 @@
 # Copyright 2015-2016 Akretion
 # (Alexis de Lattre <alexis.delattre@akretion.com>)
 # Copyright 2018 Simone Rubino - Agile Business Group
+# Copyright 2021 Tecnativa - Víctor Martínez
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 import calendar
@@ -76,6 +77,24 @@ class AccountPaymentTermLine(models.Model):
     )
     months = fields.Integer(string="Number of Months")
     weeks = fields.Integer(string="Number of Weeks")
+    value = fields.Selection(
+        selection_add=[("amount_untaxed", "Percent (Untaxed amount)")]
+    )
+    value_amount_untaxed = fields.Float(string="Untaxed amount (Percent)")
+
+    @api.constrains("value", "value_amount_untaxed")
+    def _check_value_amount_untaxed(self):
+        for term_line in self:
+            if (
+                term_line.value == "amount_untaxed"
+                and not 0 <= term_line.value_amount_untaxed <= 100
+            ):
+                raise ValidationError(
+                    _(
+                        "Percentages on the Payment Terms lines "
+                        "must be between 0 and 100."
+                    )
+                )
 
     def compute_line_amount(self, total_amount, remaining_amount, precision_digits):
         """Compute the amount for a payment term line.
@@ -92,6 +111,11 @@ class AccountPaymentTermLine(models.Model):
             return float_round(self.value_amount, precision_digits=precision_digits)
         elif self.value == "percent":
             amt = total_amount * (self.value_amount / 100.0)
+            if self.amount_round:
+                amt = float_round(amt, precision_rounding=self.amount_round)
+            return float_round(amt, precision_digits=precision_digits)
+        elif self.value == "amount_untaxed":
+            amt = total_amount * self.value_amount_untaxed / 100.0
             if self.amount_round:
                 amt = float_round(amt, precision_rounding=self.amount_round)
             return float_round(amt, precision_digits=precision_digits)
@@ -173,10 +197,10 @@ class AccountPaymentTerm(models.Model):
         return date
 
     def compute(self, value, date_ref=False, currency=None):
-        """Complete overwrite of compute method to add rounding on line
-        computing and also to handle weeks and months
-        """
+        """Complete overwrite of compute method for adding extra options."""
+        # FIXME: Find an inheritable way of doing this
         self.ensure_one()
+        last_stock_move = self.env.context.get("last_stock_move", False)
         date_ref = date_ref or fields.Date.today()
         amount = value
         result = []
@@ -187,7 +211,12 @@ class AccountPaymentTerm(models.Model):
         precision_digits = currency.decimal_places
         next_date = fields.Date.from_string(date_ref)
         for line in self.line_ids:
-            amt = line.compute_line_amount(value, amount, precision_digits)
+            if line.value == "amount_untaxed" and last_stock_move:
+                amt = line.compute_line_amount(
+                    last_stock_move.amount_untaxed, amount, precision_digits
+                )
+            else:
+                amt = line.compute_line_amount(value, amount, precision_digits)
             if not self.sequential_lines:
                 # For all lines, the beginning date is `date_ref`
                 next_date = fields.Date.from_string(date_ref)

account_payment_term_extension/tests/test_account_payment_term_multi_day.py
@@ -1,5 +1,7 @@
 import odoo.tests.common as common
 from odoo import fields
+from odoo.exceptions import ValidationError
+from odoo.tests.common import Form
 
 
 class TestAccountPaymentTermMultiDay(common.TransactionCase):
@@ -100,6 +102,74 @@ class TestAccountPaymentTermMultiDay(common.TransactionCase):
                 ],
             }
         )
+        self.amount_untaxed_lines = self.payment_term_model.create(
+            {
+                "name": "10 percent + 40 percent + Balance",
+                "active": True,
+                "sequential_lines": True,
+                "line_ids": [
+                    (
+                        0,
+                        0,
+                        {
+                            "value": "amount_untaxed",
+                            "value_amount_untaxed": 10.0,
+                            "option": "day_after_invoice_date",
+                        },
+                    ),
+                    (
+                        0,
+                        0,
+                        {
+                            "value": "amount_untaxed",
+                            "value_amount_untaxed": 40.0,
+                            "option": "day_after_invoice_date",
+                        },
+                    ),
+                    (0, 0, {"value": "balance", "option": "day_after_invoice_date"}),
+                ],
+            }
+        )
+
+    def test_amount_untaxed_payment_term_error(self):
+        payment_term_form = Form(self.payment_term_model)
+        payment_term_form.name = "10 percent + 40 percent + Balance"
+        payment_term_form.sequential_lines = True
+        with payment_term_form.line_ids.new() as line_form:
+            line_form.value = "amount_untaxed"
+            line_form.value_amount = 110
+            line_form.option = "day_after_invoice_date"
+        with self.assertRaises(ValidationError):
+            payment_term_form.save()
+
+    def test_account_invoice_with_custom_payment_term(self):
+        invoice = self.invoice_model.create(
+            {
+                "journal_id": self.journal.id,
+                "partner_id": self.partner.id,
+                "invoice_payment_term_id": self.amount_untaxed_lines.id,
+                "invoice_date": "%s-01-01" % fields.datetime.now().year,
+                "type": "in_invoice",
+                "name": "10 percent + 40 percent + Balance",
+                "invoice_line_ids": [
+                    (
+                        0,
+                        0,
+                        {
+                            "product_id": self.product.id,
+                            "name": "Test",
+                            "quantity": 10.0,
+                            "price_unit": 100.00,
+                            "account_id": self.prod_account.id,
+                        },
+                    )
+                ],
+            }
+        )
+        invoice.post()
+        self.assertEqual(invoice.line_ids[1].credit, 100.0)
+        self.assertEqual(invoice.line_ids[2].credit, 400.0)
+        self.assertEqual(invoice.line_ids[3].credit, 500.0)
 
     def test_invoice_normal_payment_term(self):
         invoice = self.invoice_model.create(

TheBusinessSolutions/accounting_18

account_payment_term_extension/models/account_move.py
@@ -1,0 +1,14 @@
+# Copyright 2021 Tecnativa Víctor Martínez
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+from odoo import models
+
+
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    def _recompute_payment_terms_lines(self):
+        super(
+            AccountMove, self.with_context(last_account_move=self)
+        )._recompute_payment_terms_lines()

account_payment_term_extension/models/account_payment_term.py
@@ -3,6 +3,7 @@
 # Copyright 2015-2016 Akretion
 # (Alexis de Lattre <alexis.delattre@akretion.com>)
 # Copyright 2018 Simone Rubino - Agile Business Group
+# Copyright 2021 Tecnativa - Víctor Martínez
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 import calendar
@@ -11,7 +12,7 @@ from functools import reduce
 from dateutil.relativedelta import relativedelta
 
 from odoo import _, api, exceptions, fields, models
-from odoo.exceptions import ValidationError
+from odoo.exceptions import UserError, ValidationError
 from odoo.tools.float_utils import float_is_zero, float_round
 
 
@@ -76,6 +77,26 @@ class AccountPaymentTermLine(models.Model):
     )
     months = fields.Integer(string="Number of Months")
     weeks = fields.Integer(string="Number of Weeks")
+    value = fields.Selection(
+        selection_add=[
+            ("percent_amount_untaxed", "Percent (Untaxed amount)"),
+            ("fixed",),
+        ]
+    )
+
+    @api.constrains("value", "value_amount")
+    def _check_value_amount_untaxed(self):
+        for term_line in self:
+            if (
+                term_line.value == "percent_amount_untaxed"
+                and not 0 <= term_line.value_amount <= 100
+            ):
+                raise ValidationError(
+                    _(
+                        "Percentages on the Payment Terms lines "
+                        "must be between 0 and 100."
+                    )
+                )
 
     def compute_line_amount(self, total_amount, remaining_amount, precision_digits):
         """Compute the amount for a payment term line.
@@ -90,8 +111,8 @@ class AccountPaymentTermLine(models.Model):
         self.ensure_one()
         if self.value == "fixed":
             return float_round(self.value_amount, precision_digits=precision_digits)
-        elif self.value == "percent":
-            amt = total_amount * (self.value_amount / 100.0)
+        elif self.value in ("percent", "percent_amount_untaxed"):
+            amt = total_amount * self.value_amount / 100.0
             if self.amount_round:
                 amt = float_round(amt, precision_rounding=self.amount_round)
             return float_round(amt, precision_digits=precision_digits)
@@ -173,21 +194,36 @@ class AccountPaymentTerm(models.Model):
         return date
 
     def compute(self, value, date_ref=False, currency=None):
-        """Complete overwrite of compute method to add rounding on line
-        computing and also to handle weeks and months
-        """
+        """Complete overwrite of compute method for adding extra options."""
+        # FIXME: Find an inheritable way of doing this
         self.ensure_one()
+        last_account_move = self.env.context.get("last_account_move", False)
         date_ref = date_ref or fields.Date.today()
         amount = value
         result = []
-        if not currency and self.env.context.get("currency_id"):
-            currency = self.env["res.currency"].browse(self.env.context["currency_id"])
-        else:
-            currency = self.env.user.company_id.currency_id
+        if not currency:
+            if self.env.context.get("currency_id"):
+                currency = self.env["res.currency"].browse(
+                    self.env.context["currency_id"]
+                )
+            else:
+                currency = self.env.user.company_id.currency_id
         precision_digits = currency.decimal_places
         next_date = fields.Date.from_string(date_ref)
         for line in self.line_ids:
-            amt = line.compute_line_amount(value, amount, precision_digits)
+            if line.value == "percent_amount_untaxed" and last_account_move:
+                if last_account_move.company_id.currency_id == currency:
+                    amount_untaxed = -last_account_move.amount_untaxed_signed
+                else:
+                    raise UserError(
+                        _(
+                            "Percentage of amount untaxed can't be used with foreign "
+                            "currencies"
+                        )
+                    )
+                amt = line.compute_line_amount(amount_untaxed, amount, precision_digits)
+            else:
+                amt = line.compute_line_amount(value, amount, precision_digits)
             if not self.sequential_lines:
                 # For all lines, the beginning date is `date_ref`
                 next_date = fields.Date.from_string(date_ref)

account_payment_term_extension/tests/test_account_payment_term_multi_day.py
@@ -1,5 +1,7 @@
 import odoo.tests.common as common
 from odoo import fields
+from odoo.exceptions import ValidationError
+from odoo.tests.common import Form
 
 
 class TestAccountPaymentTermMultiDay(common.TransactionCase):
@@ -100,6 +102,74 @@ class TestAccountPaymentTermMultiDay(common.TransactionCase):
                 ],
             }
         )
+        self.amount_untaxed_lines = self.payment_term_model.create(
+            {
+                "name": "10 percent + 40 percent + Balance",
+                "active": True,
+                "sequential_lines": True,
+                "line_ids": [
+                    (
+                        0,
+                        0,
+                        {
+                            "value": "percent_amount_untaxed",
+                            "value_amount": 10.0,
+                            "option": "day_after_invoice_date",
+                        },
+                    ),
+                    (
+                        0,
+                        0,
+                        {
+                            "value": "percent_amount_untaxed",
+                            "value_amount": 40.0,
+                            "option": "day_after_invoice_date",
+                        },
+                    ),
+                    (0, 0, {"value": "balance", "option": "day_after_invoice_date"}),
+                ],
+            }
+        )
+
+    def test_amount_untaxed_payment_term_error(self):
+        payment_term_form = Form(self.payment_term_model)
+        payment_term_form.name = "10 percent + 40 percent + Balance"
+        payment_term_form.sequential_lines = True
+        with payment_term_form.line_ids.new() as line_form:
+            line_form.value = "percent_amount_untaxed"
+            line_form.value_amount = 110
+            line_form.option = "day_after_invoice_date"
+        with self.assertRaises(ValidationError):
+            payment_term_form.save()
+
+    def test_account_invoice_with_custom_payment_term(self):
+        invoice = self.invoice_model.create(
+            {
+                "journal_id": self.journal.id,
+                "partner_id": self.partner.id,
+                "invoice_payment_term_id": self.amount_untaxed_lines.id,
+                "invoice_date": "%s-01-01" % fields.datetime.now().year,
+                "type": "in_invoice",
+                "name": "10 percent + 40 percent + Balance",
+                "invoice_line_ids": [
+                    (
+                        0,
+                        0,
+                        {
+                            "product_id": self.product.id,
+                            "name": "Test",
+                            "quantity": 10.0,
+                            "price_unit": 100.00,
+                            "account_id": self.prod_account.id,
+                        },
+                    )
+                ],
+            }
+        )
+        invoice.post()
+        self.assertEqual(invoice.line_ids[1].credit, 100.0)
+        self.assertEqual(invoice.line_ids[2].credit, 400.0)
+        self.assertEqual(invoice.line_ids[3].credit, 500.0)
 
     def test_invoice_normal_payment_term(self):
         invoice = self.invoice_model.create(