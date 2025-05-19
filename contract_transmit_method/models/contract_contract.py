7bb3e9f5eaf1f624bc6440f03d461e79c9823303
sbejaoui <souheil.bejaoui@acsone.eu>
2025-05-19 15:32:34 +0200

    [18.0][MIG] contract_transmit_method

--- contract_transmit_method/models/contract_contract.py
+++ contract_transmit_method/models/contract_contract.py
@@ -1,14 +1,14 @@
 # Copyright 2020 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, models
+from odoo import fields, models
 
 
 class ContractContract(models.Model):
-
     _inherit = "contract.contract"
 
-    @api.multi
+    transmit_method_id = fields.Many2one(tracking=True)
+
     def _prepare_invoice(self, date_invoice, journal=None):
         invoice_vals = super()._prepare_invoice(date_invoice, journal=journal)
         if self.transmit_method_id:
