acsone/contract

[IMP][REF] product_contract: rename product contract default_qty field to recurrence_number
for more clarity rename default_qty to recurrence_number

product_contract/migrations/18.0.1.0.0/pre-migrate.py
@@ -1,0 +1,17 @@
+# Copyright 2025 ACSONE SA/NV
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+from openupgradelib import openupgrade
+
+
+def migrate(cr, version):
+    openupgrade.rename_fields(
+        cr,
+        [
+            (
+                "product.template",
+                "product_template",
+                "default_qty",
+                "recurrence_number",
+            )
+        ],
+    )

TheBusinessSolutions/accounting_18

product_contract/migrations/18.0.1.0.0/pre-migrate.py
@@ -1,0 +1,18 @@
+# Copyright 2025 ACSONE SA/NV
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.rename_fields(
+        env,
+        [
+            (
+                "product.template",
+                "product_template",
+                "default_qty",
+                "recurrence_number",
+            )
+        ],
+    )