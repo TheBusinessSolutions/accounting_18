brain-tec/account-invoicing

[ADD] account_invoice_crm_tag: CRM tags on invoices
TT44457

account_invoice_crm_tag/report/account_invoice_report.py
@@ -1,0 +1,17 @@
+# Copyright 2023 Tecnativa - Stefan Ungureanu
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class AccountInvoiceReport(models.Model):
+    _inherit = "account.invoice.report"
+
+    tag_ids = fields.Many2many(
+        comodel_name="crm.tag",
+        relation="account_move_crm_tag_rel",
+        column1="account_move_id",
+        column2="crm_tag_id",
+        string="Invoice CRM Tags",
+        readonly=True,
+    )

TheBusinessSolutions/accounting_18

Co-Authored-By: Pedro M. Baeza <pedro.baeza@tecnativa.com>

account_invoice_crm_tag/report/account_invoice_report.py
@@ -1,0 +1,18 @@
+# Copyright 2023 Tecnativa - Stefan Ungureanu
+# Copyright 2023 Tecnativa - Pedro M. Baeza
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class AccountInvoiceReport(models.Model):
+    _inherit = "account.invoice.report"
+
+    crm_tag_ids = fields.Many2many(
+        comodel_name="crm.tag",
+        relation="account_move_line_crm_tag_rel",
+        column1="account_move_line_id",
+        column2="crm_tag_id",
+        string="Invoice CRM Tags",
+        readonly=True,
+    )