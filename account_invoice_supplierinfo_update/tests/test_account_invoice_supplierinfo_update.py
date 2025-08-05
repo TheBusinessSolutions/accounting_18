OCA/account-invoicing

[FIX] account_invoice_supplierinfo_update : update supplierinfo min_qty, based on new_min_quantity value on wizard line

account_invoice_supplierinfo_update/tests/test_account_invoice_supplierinfo_update.py
@@ -65,6 +65,10 @@ class Tests(TransactionCase):
         self.assertEquals(len(line_ids), 2)
         self.assertEquals(line_ids[0][2]['current_price'], False)
         self.assertEquals(line_ids[0][2]['new_price'], 400.0)
+        self.assertEquals(line_ids[0][2]['current_min_quantity'], 0.0)
+
+        # Change values
+        line_ids[0][2]['new_min_quantity'] = 6.0
 
         # Create and launch update process
         wizard = self.wizard_obj.create({
@@ -83,7 +87,7 @@ class Tests(TransactionCase):
         ])
         self.assertEquals(len(supplierinfos1), 1)
         self.assertEqual(supplierinfos1.currency_id, self.currency)
-
+        self.assertEquals(supplierinfos1.min_qty, 6.0)
         self.assertEquals(supplierinfos1.price, 400.0)
 
         supplierinfos2 = self.supplierinfo_obj.search([

TheBusinessSolutions/accounting_18

account_invoice_supplierinfo_update/tests/test_account_invoice_supplierinfo_update.py
@@ -55,10 +55,14 @@ class TestAccountMoveBlocking(AccountTestInvoicingCommon):
         self.assertEqual(line_ids[0][2]["new_price"], 400.0)
         self.assertEqual(line_ids[0][2]["current_uom_id"], False)
         self.assertEqual(line_ids[0][2]["new_uom_id"], self.uom_unit.id)
+        self.assertEqual(line_ids[0][2]["current_min_quantity"], 0.0)
         self.assertEqual(line_ids[1][2]["current_price"], False)
         self.assertEqual(line_ids[1][2]["new_price"], 10.0)
         self.assertEqual(line_ids[1][2]["current_uom_id"], False)
         self.assertEqual(line_ids[1][2]["new_uom_id"], self.uom_dozen.id)
+
+        # Change values
+        line_ids[0][2]["new_min_quantity"] = 6.0
 
         # Create and launch update process
         wizard = self.WizardUpdateSupplierinfo.create(
@@ -80,6 +84,7 @@ class TestAccountMoveBlocking(AccountTestInvoicingCommon):
         self.assertEqual(len(supplierinfo_a), 1)
         self.assertEqual(supplierinfo_a.price, 400.0)
         self.assertEqual(supplierinfo_a.product_uom, self.uom_unit)
+        self.assertEqual(supplierinfo_a.min_qty, 6.0)
 
         supplierinfo_b = self.ProductSupplierinfo.search(
             [